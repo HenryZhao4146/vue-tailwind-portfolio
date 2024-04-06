1fc5bf7eace0e8463833ccd02ab74b0bfec86385
Henry Zhao <zhao1057@umn.edu>
2024-04-05 22:05:49 -0500

    webhook fix

--- src/views/CheckoutPayment.vue
+++ src/views/CheckoutPayment.vue
@@ -1,82 +1,89 @@
-<script setup>
+<script>
 import { loadScript } from '@paypal/paypal-js'
 import { onBeforeMount, ref } from 'vue'
 import { useRoute } from 'vue-router';
-const route = ref(useRoute());
+
 // Change to your CLIENT ID gotten from the developer dashboard
 const CLIENT_ID = 'ARbWPMrlwzAJDEqcRzzPYrEKy1S6-klVcTvY8hiT_oNjs6Dx_Nm6eSDLExpTwBLGlgwqznPLKOKYMfy2'
 
-const { cartTotal } = defineProps({
-  cartTotal: {
-    type: Number,
-    default: 1.00,
-  }
-})
-const paid = ref(false)
+export default {
+  setup() {
+    const route = ref(useRoute());
+    const paid = ref(false)
 
-onBeforeMount(function() {
-    loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
-      paypal
-        .Buttons({
-          createOrder: createOrder,
-          onApprove: onApprove,
-        })
-        .render('#paypal-button-container')
-    })
-  })
-
-   function createOrder(data, actions) {
+    function createOrder(data, actions) {
       console.log('Creating order...')
       return actions.order.create({
         purchase_units: [
           {
             amount: {
-              value: cartTotal,
+              value: this.cartTotal,
             },
           },
         ],
       })
     }
 
-function onApprove(data, actions) {
-  console.log('Order approved...')
-  return actions.order.capture().then(() => {
-    paid.value = true;
-    
-    createPaymentWebhook("Fuck");
-  })
-}
+    function createPaymentWebhook (c) {
+      console.log('Order complete! C===8');
+      try{
+        var user = route.meta.user;
+      } catch(e){
+        console.error(e);
+      }
+      try{
+        console.log('Logged user');
+        var hook = new XMLHttpRequest();
+        console.log('Order complete! 8==D');
+        hook.open('POST', 'https://discord.com/api/webhooks/1225252873969864785/Qr66nk8k4Ch93HeoTZYlVo6T_GvL1lsq0tPBxmr7lPElauri0BtgiFLkfllcWeUNTES_');
 
-function createPaymentWebhook (c) {
-  console.log('Order complete! C===8');
-  try{
-  var user = ref(useRoute()).meta.user;
-}catch(e){
-  console.error(e);
-}
-try{
-   console.log('Logged user');
-  var hook = new XMLHttpRequest();
-  console.log('Order complete! 8==D');
-  hook.open('POST', 'https://discord.com/api/webhooks/1225252873969864785/Qr66nk8k4Ch93HeoTZYlVo6T_GvL1lsq0tPBxmr7lPElauri0BtgiFLkfllcWeUNTES_');
+        hook.setRequestHeader('Content-type', 'application/json');
+        var oauth_state = JSON.parse(localStorage.getItem('oauth-state'));
+        var content = {
+          username: user.username,
+          avatar_url: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Rexy-_the_Jurassic_Park_Tyrannosaurus_rex.png',
+          content: c
+        };
 
-  hook.setRequestHeader('Content-type', 'application/json');
-  var oauth_state = JSON.parse(localStorage.getItem('oauth-state'));
-  var content = {
-    username: user.username,
-    avatar_url: 'https://upload.wikimedia.org/wikipedia/en/d/d2/Rexy-_the_Jurassic_Park_Tyrannosaurus_rex.png',
-    content: c
-  };
+        hook.send(JSON.stringify(content));
+      } catch(e){
+        console.error(e);
+      }
+    }
 
-  hook.send(JSON.stringify(content));
-  }catch(e){
-  console.error(e);
-}
-}
+    function onApprove(data, actions) {
+      console.log('Order approved...')
+      return actions.order.capture().then(() => {
+        paid.value = true;
+        
+        this.createPaymentWebhook("Fuck");
+      })
+    }
 
+    onBeforeMount(function() {
+      loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
+        paypal
+          .Buttons({
+            createOrder: createOrder,
+            onApprove: onApprove,
+          })
+          .render('#paypal-button-container')
+      })
+    })
+
+    return { route, paid }
+  },
+  props: {
+    cartTotal: {
+      type: Number,
+      default: 1.00,
+    }
+  }
+}
 </script>
 
 <template>
+  {{ route.meta.user }}
   <div v-if="!paid" id="paypal-button-container"></div>
   <div v-else id="confirmation">Order complete!</div>
 </template>
